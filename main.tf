fdb7dd341f2fd06f1f5c43c1e6acb64e0162a6ca
Colin McNaughton <cloin@users.noreply.github.com>
2022-12-12 12:25:34 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+# Use AWS provider for Terraform
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "basic_rhel" {
+  ami           = "ami-06640050dc3f556bb"
+  instance_type = "t2.micro" 
+
+  }  
